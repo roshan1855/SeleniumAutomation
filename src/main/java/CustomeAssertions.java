54ca0159b703b72f248fec4413a3617155598350
roshan1855 <roshan1855@gmail.com>
2025-03-02 12:40:04 +0530

    all commit

---
+++ src/main/java/CustomeAssertions.java
@@ -0,0 +1,12 @@
+
+import org.testng.Assert;
+
+public class CustomeAssertions {
+
+        public static void verifyGreaterThan(int actual, int expected, String message) {
+            Assert.assertTrue(actual > expected, message);
+        }
+
+
+
+}
---
+++ src/main/java/StirngMethods.java
@@ -0,0 +1,13 @@
+public class StirngMethods {
+
+    public static void main(String[] args) {
+        String str = "HELLO WoRLD";
+        System.out.println(str.toLowerCase()); // Output: hello world
+
+        String str1 = "  ";
+        String str2 = "  ";
+        System.out.println(str1.isEmpty()); // Output: true
+        System.out.println(str2.isBlank()); // Output: true
+    }
+
+}
